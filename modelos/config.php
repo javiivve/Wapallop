feae92d20d92b1f3933129133d55abc1bcad18f6
javiivve <95658927+javiivve@users.noreply.github.com>
2024-01-17 18:17:04 +0100

    Update config.php

--- modelos/config.php
+++ modelos/config.php
@@ -1,14 +1,14 @@
 <?php 
-/*
+
 define('MYSQL_USER', 'alumno');
 define('MYSQL_PASS', 'alumno');
 define('MYSQL_DB', 'Wapallop');
 define('MYSQL_HOST', 'localhost');
-*/
 
+/*
 define('MYSQL_USER', 'pcbjfzkumn');
 define('MYSQL_PASS', 'H0T5YU8BDR7U0O2C$');
 define('MYSQL_DB', 'ejemploclasejvillafranca-database');
 define('MYSQL_HOST', 'ejemploclasejvillafranca-server.mysql.database.azure.com');
-
+*/
 ?>
